5ab7894cb52baae3a05fd44ed3de94525c2ffaa4
Andrew Manaster <manaster@remss.com>
2022-01-05 16:45:27 -0800

    Using Geodesic inverse for distance calculation; added multi-threading to speed up processing

--- resampling_utils/imerg_resampling_routines.py
+++ resampling_utils/imerg_resampling_routines.py
@@ -7,10 +7,13 @@ from resampling_utils.AMSR2_Antenna_Gain import *
 from pathlib import Path
 import multiprocessing
 import signal
+import concurrent.futures
+from threading import Lock
 
 
 NUM_LATS = 721
 NUM_LONS = 1440
+mutex = Lock()
 
 
 def init_worker():
@@ -53,11 +56,15 @@ def read_imerg_half_hourly(
         filename = Path(dir / file[0])
     print(filename)
 
+    mutex.acquire()  # multiple threads do not play nice opening HDF5 files
+
     hr1 = xr.open_dataset(filename, group="Grid")
     rain = hr1["precipitationCal"].values
     lat = hr1["lat"].values
     lon = hr1["lon"].values
     hr1.close()
+
+    mutex.release()
 
     return lat, lon, rain
 
@@ -67,31 +74,25 @@ def initialize_wgs84():
     Initializing WGS84 projection that we wish to project onto.
     Using this function to initialize this once saves us run time.
     """
-    crs_wgs = proj.crs.CRS("epsg:4326")  # assuming you're using WGS84 geographic
+    g = proj.Geod(ellps="WGS84")  # assuming you're using WGS84 geographic
 
-    return crs_wgs
+    return g
 
 
-def get_distance(latitude0, longitude0, ilat_submask, ilon_submask, crs_wgs):
+def get_distance(latitude0, longitude0, ilat_submask, ilon_submask, g):
     """
-    Getting a projection for distance between a central grid point
-    and an array of IMERG lats/lons. This is more accurate than
-    assuming a completely spherical Earth.
+    Calculating distance between a center point and surrounding lats/lons
+    using WGS84 geographic.
     """
 
-    # set up a custom projection, centered at latitude0,longitude0
-    cust = proj.crs.CRS(
-        f"+proj=aeqd +lat_0={latitude0} +lon_0={longitude0} +datum=WGS84 +units=m"
-    )
+    lon0_array = np.repeat(longitude0, np.size(ilon_submask))
+    lat0_array = np.repeat(latitude0, np.size(ilat_submask))
 
-    # set up a complete grid of lats/lons to be transformed.  the ilat_submask and ilon submask are 1-d arrays that define the grid.
     latv, lonv = np.meshgrid(ilat_submask, ilon_submask, indexing="ij")
+    lat0v, lon0v = np.meshgrid(lat0_array, lon0_array, indexing="ij")
+    fw_az, bk_az, dist_grid = g.inv(lon0v, lat0v, lonv, latv)
 
-    # apply the local projection to obtain the mesh points in meters from the center specified by  latitude0,longitude0
-    transformer = proj.Transformer.from_crs(crs_wgs, cust, always_xy=True)
-    xv, yv = transformer.transform(lonv, latv)
-
-    return xv, yv
+    return dist_grid
 
 
 def resample_to_quarter(map_rain, lat_rain, lon_rain, mask, window=0.5):
@@ -100,7 +101,7 @@ def resample_to_quarter(map_rain, lat_rain, lon_rain, mask, window=0.5):
     latitude and longitude, outputs resampled data on a 0.25x0.25 grid.
     """
     resampled_map = np.full((NUM_LATS, NUM_LONS), np.nan)
-    crs_wgs = initialize_wgs84()
+    g = initialize_wgs84()
 
     for i in range(0, 1440):
         for j in range(0, 721):
@@ -156,20 +157,16 @@ def resample_to_quarter(map_rain, lat_rain, lon_rain, mask, window=0.5):
                 resampled_map[j, i] = np.nan
                 continue
 
-            if (
-                ~np.any(rain != 0.0)
+            if ~np.any(
+                rain != 0.0
             ):  # no need to do a weighted average calculation if all rain rates in window are zero
                 resampled_map[j, i] = 0.0
             else:
-                xv, yv = get_distance(
-                    lat_quarter,
-                    lon_quarter,
-                    lat_rain[lat_ok],
-                    lon_rain[lon_ok],
-                    crs_wgs,
+                dist = get_distance(
+                    lat_quarter, lon_quarter, lat_rain[lat_ok], lon_rain[lon_ok], g
                 )
 
-                dist_km = ((xv / 1000.0) ** 2.0 + (yv / 1000.0) ** 2.0) ** 0.5
+                dist_km = dist / 1000.0
                 good_rain = np.where(
                     rain > -0.01
                 )  # a check based on Thomas' IMERG work. Removing any negative RR values
@@ -183,63 +180,80 @@ def resample_to_quarter(map_rain, lat_rain, lon_rain, mask, window=0.5):
     return resampled_map
 
 
+def resample_hour(hour, times, time_intervals, date, target_path):
+    sat_time = times[:, :, hour]
+
+    hour_beg = np.all(  # from beginning of hour to 30 minutes past the hour
+        [
+            (sat_time >= time_intervals[hour]),
+            (sat_time < (time_intervals[hour + 1] - 1800)),
+        ],
+        axis=(0),
+    )
+
+    hour_end = np.all(  # from 30 minutes past the hour to the following hour
+        [
+            (sat_time >= (time_intervals[hour + 1] - 1800)),
+            (sat_time < time_intervals[hour + 1]),
+        ],
+        axis=(0),
+    )
+
+    # Opening all files needed to perform resampling for given hour (need 2 IMERG files)
+    minutes_of_day_beg = int(time_intervals[hour] / 60)
+    minutes_of_day_end = int((time_intervals[hour + 1] / 60) - 30)
+
+    lat_beg, lon_beg, rain_beg = read_imerg_half_hourly(
+        minutes_of_day=minutes_of_day_beg, date=date, target_path=target_path
+    )
+
+    lat_end, lon_end, rain_end = read_imerg_half_hourly(
+        minutes_of_day=minutes_of_day_end, date=date, target_path=target_path
+    )
+
+    for_parallel = [
+        [rain_beg, lat_beg, lon_beg, hour_beg],
+        [rain_end, lat_end, lon_end, hour_end],
+    ]
+
+    # Processing 2 IMERG files for one hour in parallel
+    p = multiprocessing.Pool(5, init_worker)  # 5 workers at the moment. Can be changed
+
+    try:
+        print("Starting jobs")
+        res = p.starmap(resample_to_quarter, for_parallel)
+        print("Waiting for results")
+    except KeyboardInterrupt:
+        print("Caught KeyboardInterrupt, terminating workers")
+        p.terminate()
+    else:
+        print("Normal termination")
+        p.close()
+    p.join()  # this waits for all worker processes to terminate.
+
+    hour_map = np.nanmean((res), axis=0)
+
+    return (hour_map, hour)
+
+
 def resample_imerg_day(times, time_intervals, date, target_path=""):
     total_hour = np.full((NUM_LATS, NUM_LONS, 24), np.nan)
 
-    for idx in range(0, 24):
-        sat_time = times[:, :, idx]
-
-        hour_beg = np.all(  # from beginning of hour to 30 minutes past the hour
-            [
-                (sat_time >= time_intervals[idx]),
-                (sat_time < (time_intervals[idx + 1] - 1800)),
-            ],
-            axis=(0),
-        )
-
-        hour_end = np.all(  # from 30 minutes past the hour to the following hour
-            [
-                (sat_time >= (time_intervals[idx + 1] - 1800)),
-                (sat_time < time_intervals[idx + 1]),
-            ],
-            axis=(0),
-        )
-
-        # Opening all files needed to perform resampling for given hour (need two IMERG files)
-        minutes_of_day_beg = int(time_intervals[idx] / 60)
-        minutes_of_day_end = int((time_intervals[idx + 1] / 60) - 30)
-
-        lat_beg, lon_beg, rain_beg = read_imerg_half_hourly(
-            minutes_of_day=minutes_of_day_beg, date=date, target_path=target_path
-        )
-        lat_end, lon_end, rain_end = read_imerg_half_hourly(
-            minutes_of_day=minutes_of_day_end, date=date, target_path=target_path
-        )
-
-        for_parallel = [
-            [rain_beg, lat_beg, lon_beg, hour_beg],
-            [rain_end, lat_end, lon_end, hour_end],
-        ]
-
-        # Processing 3 IMERG files for one hour in parallel
-        p = multiprocessing.Pool(
-            10, init_worker
-        )  # 10 workers at the moment. Can be changed
-
-        try:
-            print("Starting jobs")
-            res = p.starmap(resample_to_quarter, for_parallel)
-            print("Waiting for results")
-        except KeyboardInterrupt:
-            print("Caught KeyboardInterrupt, terminating workers")
-            p.terminate()
-        else:
-            print("Normal termination")
-            p.close()
-        p.join()  # this waits for all worker processes to terminate.
-
-        total_hour[:, :, idx] = np.nanmean(
-            (res), axis=0
-        )  # this should be ok because there is, in theory, no overlap between the three resampled IMERG maps (only data on satellite orbit tracks)
+    # Using multiple threads for resampling
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        results = {
+            executor.submit(
+                resample_hour, hour, times, time_intervals, date, target_path
+            ): hour
+            for hour in range(0, 24)
+        }
+        for future in concurrent.futures.as_completed(results):
+            try:
+                (map, idx) = future.result()
+                total_hour[:, :, idx] = map
+            except KeyboardInterrupt as e:
+                return
+            except Exception as e:
+                print("Error in run.")
 
     return total_hour
